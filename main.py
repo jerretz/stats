1e6c22b58ab3627c108bbf0c1722fe6053089bd2
jerretz <jeremyretz2004@gmail.com>
2024-06-21 09:19:28 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from PyQt5.QtWidgets import *
+
+
+# from PyQt5.QtGui import QFont
+
+
+def main():
+    app = QApplication([])
+    window = QWidget()
+    window.setGeometry(500, 500, 500, 500)
+    window.setWindowTitle("Universal Interface")
+
+    button = QPushButton(window)
+    button.setText("Hhh")
+    button.move(100, 70)
+
+    layout = QVBoxLayout()
+    label = QLabel("Text")
+    layout.addWidget(label)
+    window.setLayout(layout)
+
+    window.show()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    main()
